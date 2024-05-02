3fb9a91c3f99b0fd863d42539c8f823dec9c8805
sarbajit1000 <you@example.com>
2024-05-02 22:38:50 +0530

    post detail page added

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.starting_page, name = "starting-page"),
     path("posts/", views.posts, name="posts"),
-    path("posts/<slug:sl>", views.post_details, name="post_page_details"),
+    path("posts/<slug:slug>", views.post_details, name="post_page_details"),
 ]
     
