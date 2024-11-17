5d7522c8a5b26efb9f281b86980dc6212fa3730c
sarbajit1000 <you@example.com>
2024-11-17 12:42:42 +0530

    new

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 from .models import Tag, Author, Post
 # Register your models here.
 
+
+class PostAdmin(admin.ModelAdmin):
+    list_filter=("title", "tags", "author")
+    list_display=("title", "date", "author")
+    prepopulated_fields={"slug":("title",)}
+
+    
 admin.site.register(Tag)
 admin.site.register(Author)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
